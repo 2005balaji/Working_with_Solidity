06d09a6595b7a0934ff5c8bce949eae385239cb0
Balaji S <96479581+2005balaji@users.noreply.github.com>
2023-07-06 21:46:34 +0530

    data types

---
+++ Mycontract.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyContract{
+    string public myString = "This is working with solidity";
+    bool public boolean = true;
+    uint public MyUnit = 1 ;
+    int public myInt = 1;
+}
