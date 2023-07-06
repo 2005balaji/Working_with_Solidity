72080d0671b0d8ff1c80f92b4457f5b6fa0bffde
Balaji S <96479581+2005balaji@users.noreply.github.com>
2023-07-06 22:17:32 +0530

    pay function

---
+++ pay_function.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    string public name = "Example";
+    uint256 public balance;
+
+    function getName() public view returns (string memory) {
+        return name;
+    }
+
+    // pure function caanot read name
+
+    function add(uint256 a, uint256 b) public pure returns (uint256) {
+        return a + b;
+    }
+
+    // can recive ether or crypto whenever tras. is submitted
+
+    function pay() public payable {
+        balance = msg.value;
+    }
+}
